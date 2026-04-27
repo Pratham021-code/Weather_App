eebd8d0ef9cac5e5b07cdb63ec72554f9dc64724
Pratham021-code <prathambajpai007@gmail.com>
2026-04-28 00:10:56 +0530

    Delete App.js

--- wheather/src/App.js
+++ wheather/src/App.js
@@ -1,13 +1,1 @@
-import './App.css';
-import Wheather from './wh';
 import Wh from './wh';
-
-function App() {
-  return (
-    <div className="App">
-      <Wheather/> 
-    </div>
-  );
-}
-
-export default App;
